14ba03fc13a3f036e1e5a4f5241bfaf3d0d57a2e
Anderson Fariña <inumansoul@gmail.com>
2026-05-01 12:16:18 -0300

    release v1.10.0: bundle launcher + passwordless + impersonation

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJ
 }
 
 group = "com.kauth"
-version = "1.9.0"
+version = "1.10.0"
 
 application {
     mainClass.set("com.kauth.ApplicationKt")
