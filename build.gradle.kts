2f01baf10896ed50958f334401738738d2a4f7e3
Anderson Fariña <inumansoul@gmail.com>
2026-06-22 20:54:25 -0300

    Bump version to 1.19.2, add CHANGELOG entry for docker consolidation

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJ
 }
 
 group = "com.kauth"
-version = "1.19.1"
+version = "1.19.2"
 
 application {
     mainClass.set("com.kauth.ApplicationKt")
