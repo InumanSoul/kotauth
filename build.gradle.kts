73e1af2c789814845c5ab68530b68f5d8dfba56b
Anderson Fariña <inumansoul@gmail.com>
2026-07-03 23:08:14 -0300

    release: 1.20.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJ
 }
 
 group = "com.kauth"
-version = "1.19.3"
+version = "1.20.0"
 
 application {
     mainClass.set("com.kauth.ApplicationKt")
